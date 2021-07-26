55c9187bd6b904b7654fe1f86a0ffbf2696830f5
Kostia Chaikovskyi <76705143+k4mmm@users.noreply.github.com>
2021-07-26 19:39:23 +0300

    Update apiService.js

--- src/part/apiService.js
+++ src/part/apiService.js
@@ -5,7 +5,7 @@ const API_KEY = "22603230-d9263c0bbd59724fbb3598b9f"
 export const apiService = async (request, page) => {
     try {
         const response = await axios
-            .get(`${baseURL}?key=${API_KEY}&q=${request}&image_type=photo&pretty=true&per_page=12&page=${page}`)
+            .get(`${baseURL}?key=${API_KEY}&q=${request}&image_type=photo&pretty=true&per_page=40&page=${page}`)
         return response.data
     }
     catch(error) {
